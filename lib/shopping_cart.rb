3343c1cec77fa93c47637559210e6eb57631c0d0
Robert Heath <robert.b.heathii@gmail.com>
2020-10-14 14:45:14 -0500

    Complete Iteration3

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -17,4 +17,20 @@ class ShoppingCart
   def details
     {name: @name, capacity: @capacity}
   end
+
+  def total_number_of_products
+    @products.map do |product|
+      product.quantity
+    end.sum
+  end
+
+  def is_full?
+    total_number_of_products > @capacity
+  end
+
+  def products_by_category(category)
+    @products.find_all do |product|
+      product.category == category
+    end
+  end
 end
--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -28,4 +28,44 @@ class Test < Minitest::Test
     expected = ({name: "King Soopers", capacity: 30})
     assert_equal expected, cart.details
   end
+
+  def test_total_number_of_products
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    assert_equal 13, cart.total_number_of_products
+  end
+
+  def test_if_cart_is_full?
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    assert_equal false, cart.is_full?
+
+    product4 = Product.new(:produce, 'apples', 0.99, '20')
+    cart.add_product(product4)
+    assert_equal true, cart.is_full?
+  end
+
+  def test_products_by_category
+    cart = ShoppingCart.new("King Soopers", "30items")
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    product3 = Product.new(:paper, 'tissue paper', 1.25, '1')
+    cart.add_product(product1)
+    cart.add_product(product2)
+    cart.add_product(product3)
+    expected = [product1, product3]
+    actual = cart.products_by_category(:paper)
+
+    assert_equal expected, actual
+  end
 end
